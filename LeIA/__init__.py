a4c1eeee83908581e143571324c91ab73f9bbbd0
robsonpessoa <pessoa.robsonm@gmail.com>
2022-04-03 16:55:08 -0300

    Makes repository downloadable as package LeIA directly from pip

---
+++ LeIA/__init__.py
@@ -0,0 +1,12 @@
+"""
+LeIA.
+
+LeIA (Léxico para Inferência Adaptada) é um fork do léxico e ferramenta para análise de sentimentos
+VADER (Valence Aware Dictionary and sEntiment Reasoner) adaptado para textos em português,
+com suporte para emojis e foco na análise de sentimentos de textos expressos em mídias sociais
+- mas funcional para textos de outros domínios.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Rafael J. A. Almeida'
+__credits__ = ' Núcleo de Tecnologia da Informação (NTINF) da Universidade Federal de São João del-Rei - UFSJ'
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='LeIA',
+    version='0.1.0',
+    description='LeIA (Léxico para Inferência Adaptada) é um fork do léxico e ferramenta para análise de sentimentos VADER (Valence Aware Dictionary and sEntiment Reasoner) adaptado para textos em português, com suporte para emojis e foco na análise de sentimentos de textos expressos em mídias sociais - mas funcional para textos de outros domínios.',
+    url='https://github.com/rafjaa/LeIA',
+    author='Rafael J. A. Almeida',
+    author_email='rafael@email.com',
+    license='MIT',
+    packages=['LeIA'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: Published',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
