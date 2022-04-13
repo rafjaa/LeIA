88ef804df6123e9e91c0708cbed6ee31fb83678e
robsonpessoa <pessoa.robsonm@gmail.com>
2022-04-12 23:54:46 -0300

    Adds lexicon directory to package

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='LeIA',
-    version='0.1.1',
+    version='0.1.2',
     description='LeIA (Léxico para Inferência Adaptada) é um fork do léxico e ferramenta para análise de sentimentos VADER (Valence Aware Dictionary and sEntiment Reasoner) adaptado para textos em português, com suporte para emojis e foco na análise de sentimentos de textos expressos em mídias sociais - mas funcional para textos de outros domínios.',
     url='https://github.com/rafjaa/LeIA',
     author='Rafael J. A. Almeida',
     author_email='rafael@email.com',
     license='MIT',
     packages=['LeIA'],
+    package_data={'LeIA': ['lexicons/*.txt']},
     install_requires=[],
 
     classifiers=[
@@ -17,6 +18,5 @@ setup(
         'License :: OSI Approved :: MIT',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
     ],
 )
